b25299cebbd02180544426cf31e0660c425cdcb2
Ekene <megafat142@gmail.com>
2023-11-15 10:59:12 +0100

    drizzle why

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,37 +1,10 @@
 
 
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
 import { env } from "@/env.mjs";
-
-// Schema imports
-import * as schema from './schema';
-
-let dbInstance: typeof drizzle | null | any = null;
-
-declare global {
-  const _drizzleDb: typeof drizzle | undefined | any;
-}
-
-if (process.env.NODE_ENV === 'production') {
-
-  const client = postgres(env.DATABASE_URL);
-
-  dbInstance = drizzle(client, {
-    schema
-  });
-
-} else {
-
-  if (!global._drizzleDb) {
-    const client = postgres(env.DATABASE_URL);
-    
-    global._drizzleDb = drizzle(client, {
-      schema
-    });
-  }
-
-  dbInstance = global._drizzleDb as any; 
-}
-
-export { dbInstance as db };
+import * as schema1 from './schema'
+ 
+const connectionString = env.DATABASE_URL
+const client = postgres(connectionString)
+export const db = drizzle(client, {schema:{...schema1}});
