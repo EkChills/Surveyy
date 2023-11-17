b707ad1bc8a1ae3105bda220f0d7282d7ef6592d
Ekene <megafat142@gmail.com>
2023-11-17 15:55:30 +0100

    edge

--- src/app/api/createSurvey/route.ts
+++ src/app/api/createSurvey/route.ts
@@ -3,7 +3,7 @@ import { SurveyResultsSchema,type SurveyResultsType,type createInputSchemaTypes,
 import {NextResponse, type NextRequest } from "next/server";
 import {v4 as uuid} from 'uuid'
 
-
+export const runtime = "edge"
 
 export async function POST (req:NextRequest) {
   console.log('triggered');
