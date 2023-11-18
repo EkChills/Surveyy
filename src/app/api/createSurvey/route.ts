4a59649bb44d31580767124b404206b18ae65b3d
Ekene <megafat142@gmail.com>
2023-11-18 10:09:12 +0100

    edge way8

--- src/app/api/createSurvey/route.ts
+++ src/app/api/createSurvey/route.ts
@@ -3,9 +3,9 @@ import { SurveyResultsSchema,type SurveyResultsType,type createInputSchemaTypes,
 import {NextResponse, type NextRequest } from "next/server";
 import {v4 as uuid} from 'uuid'
 
-export const runtime = "edge"
-export const dynamic = 'force-dynamic';
-export const maxDuration = 30
+// export const runtime = "edge"
+// export const dynamic = 'force-dynamic';
+// export const maxDuration = 30
 
 export async function POST (req:NextRequest) {
   console.log('triggered');
