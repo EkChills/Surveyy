5ced8ba1a725b6acabc31ff8d4f81da6f16bed45
Ekene <megafat142@gmail.com>
2023-11-22 17:06:39 +0100

    responsive fixes

--- src/components/OverviewHero.tsx
+++ src/components/OverviewHero.tsx
@@ -12,7 +12,7 @@ export async function OverviewHero({surveyId}:{surveyId:string}) {
         <>
         <span className='p-2 rounded-xl bg-[#EEFBE4] font-semibold text-sm text-[#5F843A]'>Active</span>
         <h1 className='text-center text-lg sm:text-2xl lg:text-3xl xl:text-4xl font-semibold antialiased'>{surveyTitle[0]?.name}</h1>
-        <span className={cn('rounded-lg border flex items-center max-w-[26rem]', buttonVariants({variant:'outline'}))}><span className='text-sm md:text-base font-semibold text-[#1B324D] truncate'>{copyUrl}</span><CopyLink copyText={copyUrl} /></span>
+        <span className={cn('rounded-lg border flex items-center max-w-full sm:max-w-[26rem] ', buttonVariants({variant:'outline'}))}><span className='text-sm md:text-base font-semibold text-[#1B324D] truncate'>{copyUrl}</span><CopyLink copyText={copyUrl} /></span>
         </>
         
     )
